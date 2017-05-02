9724284f5a01fb83c34286f0b92b7054d859fd5f
RickGray <rickchen.vip@gmail.com>
2017-05-02 15:36:28 +0800

    use "github.com/zmap/zcrypto/tls" replace of "crypto/tls"

--- zgrab-mini.go
+++ zgrab-mini.go
@@ -11,37 +11,39 @@ import (
     "sync"
     "bufio"
     "strings"
-    
+
     "encoding/json"
-    "crypto/x509"
-    "crypto/tls"
+    //"crypto/x509"
+    //"crypto/tls"
+    //"github.com/zmap/zcrypto/x509"
+    "github.com/zmap/zcrypto/tls"
 )
 
-const rootPEM = `
------BEGIN CERTIFICATE-----
-MIIEBDCCAuygAwIBAgIDAjppMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT
-MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i
-YWwgQ0EwHhcNMTMwNDA1MTUxNTU1WhcNMTUwNDA0MTUxNTU1WjBJMQswCQYDVQQG
-EwJVUzETMBEGA1UEChMKR29vZ2xlIEluYzElMCMGA1UEAxMcR29vZ2xlIEludGVy
-bmV0IEF1dGhvcml0eSBHMjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-AJwqBHdc2FCROgajguDYUEi8iT/xGXAaiEZ+4I/F8YnOIe5a/mENtzJEiaB0C1NP
-VaTOgmKV7utZX8bhBYASxF6UP7xbSDj0U/ck5vuR6RXEz/RTDfRK/J9U3n2+oGtv
-h8DQUB8oMANA2ghzUWx//zo8pzcGjr1LEQTrfSTe5vn8MXH7lNVg8y5Kr0LSy+rE
-ahqyzFPdFUuLH8gZYR/Nnag+YyuENWllhMgZxUYi+FOVvuOAShDGKuy6lyARxzmZ
-EASg8GF6lSWMTlJ14rbtCMoU/M4iarNOz0YDl5cDfsCx3nuvRTPPuj5xt970JSXC
-DTWJnZ37DhF5iR43xa+OcmkCAwEAAaOB+zCB+DAfBgNVHSMEGDAWgBTAephojYn7
-qwVkDBF9qn1luMrMTjAdBgNVHQ4EFgQUSt0GFhu89mi1dvWBtrtiGrpagS8wEgYD
-VR0TAQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAQYwOgYDVR0fBDMwMTAvoC2g
-K4YpaHR0cDovL2NybC5nZW90cnVzdC5jb20vY3Jscy9ndGdsb2JhbC5jcmwwPQYI
-KwYBBQUHAQEEMTAvMC0GCCsGAQUFBzABhiFodHRwOi8vZ3RnbG9iYWwtb2NzcC5n
-ZW90cnVzdC5jb20wFwYDVR0gBBAwDjAMBgorBgEEAdZ5AgUBMA0GCSqGSIb3DQEB
-BQUAA4IBAQA21waAESetKhSbOHezI6B1WLuxfoNCunLaHtiONgaX4PCVOzf9G0JY
-/iLIa704XtE7JW4S615ndkZAkNoUyHgN7ZVm2o6Gb4ChulYylYbc3GrKBIxbf/a/
-zG+FA1jDaFETzf3I93k9mTXwVqO94FntT0QJo544evZG0R0SnU++0ED8Vf4GXjza
-HFa9llF7b1cq26KqltyMdMKVvvBulRP/F/A8rLIQjcxz++iPAsbw+zOzlTvjwsto
-WHPbqCRiOwY1nQ2pM714A5AuTHhdUDqB1O6gyHA43LL5Z/qHQF1hwFGPa4NrzQU6
-yuGnBXj8ytqU0CwIPX4WecigUCAkVDNx
------END CERTIFICATE-----`
+//const rootPEM = `
+//-----BEGIN CERTIFICATE-----
+//MIIEBDCCAuygAwIBAgIDAjppMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT
+//MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i
+//YWwgQ0EwHhcNMTMwNDA1MTUxNTU1WhcNMTUwNDA0MTUxNTU1WjBJMQswCQYDVQQG
+//EwJVUzETMBEGA1UEChMKR29vZ2xlIEluYzElMCMGA1UEAxMcR29vZ2xlIEludGVy
+//bmV0IEF1dGhvcml0eSBHMjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
+//AJwqBHdc2FCROgajguDYUEi8iT/xGXAaiEZ+4I/F8YnOIe5a/mENtzJEiaB0C1NP
+//VaTOgmKV7utZX8bhBYASxF6UP7xbSDj0U/ck5vuR6RXEz/RTDfRK/J9U3n2+oGtv
+//h8DQUB8oMANA2ghzUWx//zo8pzcGjr1LEQTrfSTe5vn8MXH7lNVg8y5Kr0LSy+rE
+//ahqyzFPdFUuLH8gZYR/Nnag+YyuENWllhMgZxUYi+FOVvuOAShDGKuy6lyARxzmZ
+//EASg8GF6lSWMTlJ14rbtCMoU/M4iarNOz0YDl5cDfsCx3nuvRTPPuj5xt970JSXC
+//DTWJnZ37DhF5iR43xa+OcmkCAwEAAaOB+zCB+DAfBgNVHSMEGDAWgBTAephojYn7
+//qwVkDBF9qn1luMrMTjAdBgNVHQ4EFgQUSt0GFhu89mi1dvWBtrtiGrpagS8wEgYD
+//VR0TAQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAQYwOgYDVR0fBDMwMTAvoC2g
+//K4YpaHR0cDovL2NybC5nZW90cnVzdC5jb20vY3Jscy9ndGdsb2JhbC5jcmwwPQYI
+//KwYBBQUHAQEEMTAvMC0GCCsGAQUFBzABhiFodHRwOi8vZ3RnbG9iYWwtb2NzcC5n
+//ZW90cnVzdC5jb20wFwYDVR0gBBAwDjAMBgorBgEEAdZ5AgUBMA0GCSqGSIb3DQEB
+//BQUAA4IBAQA21waAESetKhSbOHezI6B1WLuxfoNCunLaHtiONgaX4PCVOzf9G0JY
+///iLIa704XtE7JW4S615ndkZAkNoUyHgN7ZVm2o6Gb4ChulYylYbc3GrKBIxbf/a/
+//zG+FA1jDaFETzf3I93k9mTXwVqO94FntT0QJo544evZG0R0SnU++0ED8Vf4GXjza
+//HFa9llF7b1cq26KqltyMdMKVvvBulRP/F/A8rLIQjcxz++iPAsbw+zOzlTvjwsto
+//WHPbqCRiOwY1nQ2pM714A5AuTHhdUDqB1O6gyHA43LL5Z/qHQF1hwFGPa4NrzQU6
+//yuGnBXj8ytqU0CwIPX4WecigUCAkVDNx
+//-----END CERTIFICATE-----`
 
 type Config struct {
     Senders uint
@@ -78,8 +80,9 @@ var (
     maxReadLength  uint
 )
 
-type TLSHandshake struct {
-}
+//type TLSHandshake struct {
+//    State tls.ConnectionState   `json:"state"`
+//}
 
 type GrabTarget struct {
     IP   string   `json:"ip"`
@@ -99,10 +102,10 @@ type GrabResult struct {
 }
 
 type GrabData struct {
-    Banner       string        `json:"banner"`
-    IsTLS        bool          `json:"is_tls"`
-    TLSHandshake TLSHandshake  `json:"tls"`
-    Component    string        `json:"component"`
+    Banner       string                `json:"banner"`
+    IsTLS        bool                  `json:"is_tls"`
+    TLSHandshake *tls.ServerHandshake  `json:"tls"`
+    Component    string                `json:"component"`
 }
 
 type GrabWorker struct {
@@ -194,13 +197,13 @@ func GrabBannerHTTPS(c *Config, t *GrabTarget) (data GrabData, err error) {
     address := t.GetAddress()
     dialer := MakeDialer(c)
 
-    roots := x509.NewCertPool()
-    ok := roots.AppendCertsFromPEM([]byte(rootPEM))
-    if !ok {
-        return data, err
-    }
+    //roots := x509.NewCertPool()
+    //ok := roots.AppendCertsFromPEM([]byte(rootPEM))
+    //if !ok {
+    //    return data, err
+    //}
     conf := tls.Config{
-        RootCAs:            roots,
+        //RootCAs:            roots,
         InsecureSkipVerify: true,
     }
     tlsConn, err := tls.DialWithDialer(&dialer, "tcp", address, &conf)
@@ -223,11 +226,13 @@ func GrabBannerHTTPS(c *Config, t *GrabTarget) (data GrabData, err error) {
     }
 
     // TODO: parse TLS infomations
-    //tlsConn.ConnectionState()
+    //stateJSON, err := json.Marshal(tlsConn.ConnectionState())
+    //if err != nil {
+    //    fmt.Println(string(stateJSON))
+    //}
     //data.TLSHandshake = xxxxx
+    data.TLSHandshake = tlsConn.GetHandshakeLog()
     data.IsTLS = true
-
-    tlsConn.ConnectionState()
 
     data.Banner = string(buff[:n])
     data.Component = "https"
